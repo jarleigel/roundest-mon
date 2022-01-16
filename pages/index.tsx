c01f7ff57cc2a35adf81697e9ca1ecb18f507679
jarleigel <jarleigel@gmail.com>
2022-01-16 20:33:31 +0000

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div className="text-2xl text-red-500">Hallaballa</div>
+  return <div className="h-screen w-screen flex flex-col justify-center items-center">
+    <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
+    <div className="p-2" />
+    <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+      <div className="w-16 h-16 bg-red-200"></div>
+      <div className="p-8">Vs</div>
+      <div className="w-16 h-16 bg-red-200"></div>
+    </div>
+  </div>
 }
